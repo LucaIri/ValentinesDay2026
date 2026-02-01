ecbf702cf4bbc11c26f53bb5081092610fc2d087
Luca Irimie <lucaniri@umich.edu>
2026-01-31 21:16:40 -0500

    bark sequence

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const headerText = document.getElementById("header-text");
 const screen1 = document.getElementById("screen-1");
 const screen2 = document.getElementById("screen-2");
 
-const barkSound = document.getElementById("bark-sound");
+const barkSound1 = document.getElementById("bark-sound-1");
+const barkSound2 = document.getElementById("bark-sound-2");
 
 // Optional bark loop
 setInterval(() => {
-  barkSound.currentTime = 0;
-  barkSound.play();
-}, 3000);
+  barkSound1.currentTime = 0;
+  barkSound1.play();
+  barkSound2.currentTime = 500;
+  barkSound2.play();
+}, 2000);
 
 // Click Goose → Screen 2
 goose.addEventListener("click", () => {
@@ -27,6 +30,8 @@ const screen3 = document.getElementById("screen-3");
 
 // Click scroll → reveal letter
 gooseScroll.addEventListener("click", () => {
+  barkSound1.pause();
+  barkSound2.pause();
   screen2.classList.add("hidden");
   screen3.classList.remove("hidden");
 });
