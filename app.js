331544c67c6d336b46eb77c174b8a6541de1f0b8
Luca Irimie <lucaniri@umich.edu>
2026-01-31 23:30:30 -0500

    random move

--- app.js
+++ app.js
@@ -64,3 +64,18 @@ gooseScroll.addEventListener("click", () => {
     barkInterval = null;
   }
 });
+
+const noBtn = document.getElementById("no-btn");
+
+noBtn.addEventListener("click", () => {
+  const padding = 50;
+
+  const maxX = window.innerWidth - noBtn.offsetWidth - padding;
+  const maxY = window.innerHeight - noBtn.offsetHeight - padding;
+
+  const randomX = Math.random() * maxX;
+  const randomY = Math.random() * maxY;
+
+  noBtn.style.left = `${randomX}px`;
+  noBtn.style.top = `${randomY}px`;
+});
